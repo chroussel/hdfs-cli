ac7fffdcfe3bec04a943caaf5aef26d6f5fe00f4
Charles Roussel <c.roussel@criteo.com>
2018-11-09 01:30:36 +0100

    again

--- build_utils/googletest.sh
+++ build_utils/googletest.sh
@@ -2,15 +2,10 @@
 set +ev
 
 mkdir -p build
-(
-    cd build
-    git clone https://github.com/ContinuumIO/libhdfs3-downstream.git libhdfs3
-    cd libhdfs3/libhdfs3
-    mkdir -p build
-    (
-        cd build
-        cmake ..
-        make && make install
-    )
-)
-sudo cp -a build/libhdfs3/libhdfs3/build/* libhdfs3/.
+cd build
+wget https://github.com/google/googletest/archive/release-1.8.1.tar.gz
+tar xf release-1.8.1.tar.gz
+cd googletest-release-1.8.1
+cmake .
+make
+sudo make install
