6bf4d23b78c63d129b03994238dffa88f06f7611
Charles Roussel <c.roussel@criteo.com>
2018-11-09 01:10:59 +0100

    fix config

--- build_utils/googletest.sh
+++ build_utils/googletest.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/sh +e
 
 mkdir -p build
 cd build
-sudo wget https://github.com/google/googletest/archive/release-1.8.1.tar.gz
-sudo tar xf release-1.8.1.tar.gz
+wget https://github.com/google/googletest/archive/release-1.8.1.tar.gz
+tar xf release-1.8.1.tar.gz
 cd googletest-release-1.8.1
-sudo cmake
-sudo make
-sudo cp -a include/gtest /usr/include
-sudo cp -a libgtest_main.so libgtest.so /usr/lib/
+cmake .
+make
+sudo make install
--- build_utils/libhdfs3.sh
+++ build_utils/libhdfs3.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/sh +e
 
 mkdir -p build
 cd build
 git clone https://github.com/ContinuumIO/libhdfs3-downstream.git libhdfs3
 cd libhdfs3/libhdfs3
-cmake
+mkdir -p build
+cd build
+cmake ..
+make && make install
 sudo cp -a dist/include /usr/include
 sudo cp -a dist/lib /usr/lib/
