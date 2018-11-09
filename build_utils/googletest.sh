157ebe7067b5c97eb02c008a3a05afc6101d2960
Charles Roussel <c.roussel@criteo.com>
2018-11-09 01:32:37 +0100

    Fix wrong file modified

--- build_utils/googletest.sh
+++ build_utils/googletest.sh
@@ -2,10 +2,12 @@
 set +ev
 
 mkdir -p build
-cd build
-wget https://github.com/google/googletest/archive/release-1.8.1.tar.gz
-tar xf release-1.8.1.tar.gz
-cd googletest-release-1.8.1
-cmake .
-make
-sudo make install
+(
+    cd build
+    wget https://github.com/google/googletest/archive/release-1.8.1.tar.gz
+    tar xf release-1.8.1.tar.gz
+    cd googletest-release-1.8.1
+    cmake .
+    make
+    sudo make install
+)
--- build_utils/libhdfs3.sh
+++ build_utils/libhdfs3.sh
@@ -2,12 +2,15 @@
 set +ev
 
 mkdir -p build
-cd build
-git clone https://github.com/ContinuumIO/libhdfs3-downstream.git libhdfs3
-cd libhdfs3/libhdfs3
-mkdir -p build
-cd build
-cmake ..
-make && make install
-sudo cp -a dist/include /usr/include
-sudo cp -a dist/lib /usr/lib/
+(
+    cd build
+    git clone https://github.com/ContinuumIO/libhdfs3-downstream.git libhdfs3
+    cd libhdfs3/libhdfs3
+    mkdir -p build
+    (
+        cd build
+        cmake ..
+        make && make install
+    )
+)
+sudo cp -a build/libhdfs3/libhdfs3/build/* libhdfs3/.
