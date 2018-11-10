d4d5d691f5fb400b44f5ce795e83246e7a3dcf4d
Charles Roussel <c.roussel@criteo.com>
2018-11-11 00:16:43 +0100

    fix execution

--- build_utils/libhdfs3.sh
+++ build_utils/libhdfs3.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 set +ev
 
+num_proc=1
+# If PARALLEL_BUILD environment variable is set then parallel build is enabled
+if [ "${PARALLEL_BUILD:-}" != "" ]; then
+    # If PARALLEL_BUILD content is a number then use it as number of parallel jobs
+    if [ ! -z "${PARALLEL_BUILD##*[!0-9]*}" ]; then
+        num_proc=${PARALLEL_BUILD}
+    else
+        # Try to determine the number of available CPUs
+        if command_exists nproc; then
+            num_proc=$(nproc)
+            elif command_exists sysctl; then
+            num_proc=$(sysctl -n hw.ncpu)
+        fi
+    fi
+fi
+
 mkdir -p build
 (
     cd build
@@ -10,7 +26,7 @@ mkdir -p build
     (
         cd build
         cmake ..
-        make -j 4
+        make -j $(num_proc)
         sudo make install
     )
 )
